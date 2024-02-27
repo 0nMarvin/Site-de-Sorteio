211264510612f38761951d2a94e21668dc0acd07
JPMontilha <jpvacilotomontilha@gmail.com>
2024-02-27 15:25:26 -0300

    feat: adição da função e exeplo para criação da lista de quem será sorteado

---
+++ Back/back.js
@@ -0,0 +1,40 @@
+function sorteio(max){
+    if (max == 0){
+        console.error("Insira um número maior que 0 para ser sorteado");
+    return;
+    }
+
+    let sorteado = [];
+
+    while (sorteado.length < max){
+        let nominho = Math.floor(Math.random() * max);
+
+        if(!sorteado.includes(nominho))
+        sorteado.push(nominho);
+    }
+
+    return sorteado;
+}
+
+function montaLista(nome, email, i, lista){
+    lista.push({index: i, nome: nome, email: email});
+    return lista;
+}
+
+let lista = [];
+let index = 0;
+lista = montaLista("João", "emeil pika", index, lista);
+index++;
+lista = montaLista("Marcus", "emeil criativo", index, lista);
+index++;
+lista = montaLista("Luquinhas <3", "emeil foda", index, lista);
+index++;
+lista = montaLista("Joice", "emeil feminino", index, lista);
+index++;
+lista = montaLista("Felippe", "emeil depressivo", index, lista);
+index++;
+console.log(lista)
+
+
+let teste = sorteio(lista.length);
+console.log(teste);
