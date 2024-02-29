c2d304bc6123e5b2ac613fb79f853a94084c7456
JPMontilha <jpvacilotomontilha@gmail.com>
2024-02-29 16:25:52 -0300

    feat: adição da função de enviar e-mails

--- Back/back.js
+++ Back/back.js
@@ -1,13 +1,10 @@
+const nodemailer = require('nodemailer');
+
 function sorteio(max){
-    if (max == 0){
+    if (max <= 0){
         console.error("Insira um número maior que 0 para ser sorteado");
     return;
     }
-
-    // let evitar = [];
-    // for (let i = 0; i < max; i++){
-    //     evitar.push(i);
-    // }
 
     let sorteado = [];
     
@@ -29,20 +26,66 @@ function montaLista(nome, email, i, lista){
     return lista;
 }
 
+function enviarEmail(destino, resultado){
+    // Configurações do transporte para o Hotmail
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: '',
+            pass: ''
+        },
+    });
+    
+    // Opções do e-mail
+    let mailOptions = {
+        from: '',
+        to: destino,
+        subject: 'Sorte.IO',
+        html: `<h1> Aqui está seu sorteio </h1> <p>Você sorteou o(a) ${resultado}</p>`,
+        text: `Aqui está seu sorteio. Você sorteou o(a) ${resultado}`
+    };
+    
+    // Enviar e-mail
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.error('Erro ao enviar e-mail:', error);
+    }
+    console.log('E-mail enviado com sucesso:', info.response);
+    });
+}
+
 let lista = [];
 let index = 0;
-lista = montaLista("João", "emeil pika", index, lista);
+lista = montaLista("João", "", index, lista);
 index++;
-lista = montaLista("Marcus", "emeil criativo", index, lista);
+lista = montaLista("Marcus", "", index, lista);
 index++;
-lista = montaLista("Luquinhas <3", "emeil foda", index, lista);
+lista = montaLista("Luquinhas <3", "", index, lista);
 index++;
-lista = montaLista("Joice", "emeil feminino", index, lista);
+lista = montaLista("Joice", "", index, lista);
 index++;
-lista = montaLista("Felippe", "emeil depressivo", index, lista);
+lista = montaLista("Felippe", "", index, lista);
 index++;
 console.log(lista)
 
 
 let teste = sorteio(lista.length);
 console.log(teste);
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function executeDelayedCode() {
+    for (let i=0; i<lista.length; i++) {
+        console.log(`Enviando email ${i+1}`)
+        enviarEmail(lista[i].email, lista[teste[i]].nome);
+  
+        // Pause execution for 5 seconds (5000 milliseconds)
+        await delay(5000);
+    }
+  }
+
+  executeDelayedCode();
